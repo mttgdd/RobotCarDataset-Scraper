0097d5f089f4e1fbcb62c61003b8c1d03d46d820
Matt Gadd <mattgadd@robots.ox.ac.uk>
2019-03-14 18:34:20 +0000

    tidy up copyright

--- scrape_mrgdatashare.py
+++ scrape_mrgdatashare.py
@@ -3,19 +3,6 @@
 '''
 Downloads files matching patterns from the Oxford Robotcar Dataset website.
 
-=========================== MRG Copyright Header ===========================
-
-Copyright (c) 2003-2019 University of Oxford. All rights reserved.
-Authors: Mobile Robotics Group, University of Oxford
-          http://mrg.robots.ox.ac.uk
-
- This file is the property of the University of Oxford.
- Redistribution and use in source and binary forms, with or without
- modification, is not permitted without an explicit licensing agreement
- (research or commercial). No warranty, explicit or implicit, provided.
-
-=========================== MRG Copyright Header ===========================
-
 Matt Gadd
 Jan 2018
 Oxford Robotics Institute, Oxford University.
